e9874235daab57a51bb4573330e40675aa565c67
bineanshi <bineanshi@163.com>
2023-12-29 16:31:00 +0800

    add entgo hooks

---
+++ ent_orm/hooks/id_hook.go
@@ -0,0 +1,29 @@
+package hooks
+
+import (
+	"context"
+	"entgo.io/ent"
+	"fmt"
+	"github.com/sony/sonyflake"
+)
+
+func IDHook() ent.Hook {
+	sf := sonyflake.NewSonyflake(sonyflake.Settings{})
+	type IDSetter interface {
+		SetID(uint64)
+	}
+	return func(next ent.Mutator) ent.Mutator {
+		return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+			is, ok := m.(IDSetter)
+			if !ok {
+				return nil, fmt.Errorf("unexpected mutation %T", m)
+			}
+			id, err := sf.NextID()
+			if err != nil {
+				return nil, err
+			}
+			is.SetID(id)
+			return next.Mutate(ctx, m)
+		})
+	}
+}
